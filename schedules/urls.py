7ad5eedf69ed71aa77ff29b8a6994c71f8e91ea5
Zyginte <zyginte.m@gmail.com>
2024-06-04 21:58:46 +0300

    Added week table to the home.html

--- schedules/urls.py
+++ schedules/urls.py
@@ -4,5 +4,5 @@ from .views import home, user_availability
 #Nurodome marsrutus ir puslapius
 urlpatterns = [
     path('', home, name='home'),
-    path('schedule/', user_availability, name='schedule'), #Marsrutas, puslapis is views (importuojame), pavadinimas (jei noresime kurti pvz. mygtukus, kurie nukreipia i sita puslapi)
+    path('schedule/<str:schedule_format>/', user_availability, name='user_availability'), #Marsrutas, puslapis in views (importuojame), pavadinimas (jei noresime kurti pvz. mygtukus, kurie nukreipia i sita puslapi)
 ]
--- schedules/views.py
+++ schedules/views.py
@@ -2,54 +2,88 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from schedules.models import Availability, Vacation, CustomUser
 from django.utils import timezone
+from datetime import timedelta, datetime
 
 # Create your views here.
 
 def home(request):
+    today = datetime.now().date()
+    start_date = today - timedelta(days=today.weekday())  # Monday
+    date_range = [start_date + timedelta(days=i) for i in range(7)]
+    
     users = CustomUser.objects.all()
+    
     data = {
         'users': users,
+        'date_range': date_range,
     }
     return render(request, 'home.html', context=data)
 
-def user_availability(request):
+
+def user_availability(request, schedule_format='week'):
     today = timezone.now().date()
+
+    if schedule_format == 'day':
+        start_date = today
+        end_date = today
+        date_range = [start_date]
+    elif schedule_format == 'week':
+        start_date = today - timedelta(days=today.weekday())  # Monday
+        end_date = start_date + timedelta(days=6)  # Sunday
+        date_range = [start_date + timedelta(days=i) for i in range(7)]
+    elif schedule_format == 'month':
+        start_date = today.replace(day=1)  # First day of the month
+        next_month = start_date.month % 12 + 1
+        end_date = start_date.replace(month=next_month, day=1) - timedelta(days=1)  # Last day of the month
+        date_range = [start_date + timedelta(days=i) for i in range((end_date - start_date).days + 1)]
+    else:
+        return HttpResponse("Invalid schedule format", status=400)
+
     users = CustomUser.objects.all()
-    availabilities_today = Availability.objects.filter(day=today)
-    user_vacation = Vacation.objects.filter(first_day__lte=today, last_day__gte=today)
+    availabilities = Availability.objects.filter(day__range=[start_date, end_date])
+    user_vacations = Vacation.objects.filter(first_day__lte=end_date, last_day__gte=start_date)
 
-    vacation_map = {vacation.user_id: vacation for vacation in user_vacation}
+    vacation_map = {vacation.user_id: vacation for vacation in user_vacations}
 
-    availability_map = {availability.user_id: availability for availability in availabilities_today}
+    availability_map = {}
+    for availability in availabilities:
+        if availability.user_id not in availability_map:
+            availability_map[availability.user_id] = []
+        availability_map[availability.user_id].append(availability)
 
     users_on_vacation = []
     available_users = []
     not_available_users = []
 
     for user in users:
-        if user.id in availability_map:
-            availability = availability_map[user.id]
-            available_from = availability.start_time
-            available_until = availability.end_time
-
+        user_available_days = []
+        user_vacation_days = []
+        for day in date_range:
+            if user.id in availability_map:
+                for availability in availability_map[user.id]:
+                    if availability.day == day:
+                        user_available_days.append({
+                            'day': day,
+                            'available_from': availability.start_time,
+                            'available_until': availability.end_time,
+                        })
+            elif user.id in vacation_map and vacation_map[user.id].first_day <= day <= vacation_map[user.id].last_day:
+                user_vacation_days.append({
+                    'day': day,
+                    'type': vacation_map[user.id].get_type_display(),
+                })
+        
+        if user_available_days:
             available_users.append({
                 'user': user,
-                'available_from': available_from,
-                'available_until': available_until,
+                'available_days': user_available_days,
             })
-        elif user.id in vacation_map:
-            on_vacation = vacation_map[user.id]
-            on_vacation_from = on_vacation.first_day
-            on_vacation_until = on_vacation.last_day
-
-            not_available_users.append({
+        elif user_vacation_days:
+            users_on_vacation.append({
                 'user': user,
-                'on_vacation_from': on_vacation_from,
-                'on_vacation_until': on_vacation_until,
-                'type': on_vacation.get_type_display(),
+                'vacation_days': user_vacation_days,
             })
         else:
-            # If neither available nor on vacation, add to not_available_users
             not_available_users.append({
                 'user': user,
             })
@@ -58,7 +92,7 @@ def user_availability(request):
         'available_users': available_users,
         'not_available_users': not_available_users,
         'users_on_vacation': users_on_vacation,
-        'today': today,
+        'date_range': date_range,
         'hours_range': range(24)
     }
 
